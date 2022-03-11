03af076074ce569bd155d33dcd00c69809ac3e0a
fahadsamman <7100398+fahadsamman@users.noreply.github.com>
2022-03-11 20:20:43 +0300

    Used pyqtgraph library to establish live EEG plotting

--- Main.py
+++ Main.py
@@ -1,60 +1,130 @@
 
+
 import argparse
 import time
+import logging
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import brainflow
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, LogLevels, BoardIds
-from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
+from brainflow.data_filter import DataFilter, FilterTypes, AggOperations, DetrendOperations
+
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
 
 
-BoardShim.enable_dev_board_logger()
+class Graph:
+    def __init__(self, board_shim):
+        self.board_id = board_shim.get_board_id()
+        self.board_shim = board_shim
+        self.exg_channels = BoardShim.get_exg_channels(self.board_id)
+        self.sampling_rate = BoardShim.get_sampling_rate(self.board_id)
+        self.update_speed_ms = 50
+        self.window_size = 4
+        self.num_points = self.window_size * self.sampling_rate
 
-params = BrainFlowInputParams()
-params.ip_port = 0
-params.serial_port = '/dev/cu.usbserial-DQ007SNX'
-params.mac_address = ''
-params.other_info = ''
-params.serial_number = ''
-params.ip_address = ''
-params.ip_protocol = 0
-params.timeout = 0
-params.file = ''
+        self.app = QtGui.QApplication([])
+        self.win = pg.GraphicsWindow(title='BrainFlow Plot',size=(800, 600))
+
+        self._init_timeseries()
+
+        timer = QtCore.QTimer()
+        timer.timeout.connect(self.update)
+        timer.start(self.update_speed_ms)
+        QtGui.QApplication.instance().exec_()
 
 
-board = BoardShim(0, params)
+    def _init_timeseries(self):
+        self.plots = list()
+        self.curves = list()
+        for i in range(len(self.exg_channels)):
+            p = self.win.addPlot(row=i,col=0)
+            p.showAxis('left', False)
+            p.setMenuEnabled('left', False)
+            p.showAxis('bottom', False)
+            p.setMenuEnabled('bottom', False)
+            if i == 0:
+                p.setTitle('TimeSeries Plot')
+            self.plots.append(p)
+            curve = p.plot()
+            self.curves.append(curve)
+
+    def update(self):
+        data = self.board_shim.get_current_board_data(self.num_points)
+        for count, channel in enumerate(self.exg_channels):
+            # plot timeseries
+            DataFilter.detrend(data[channel], DetrendOperations.CONSTANT.value)
+            DataFilter.perform_bandpass(data[channel], self.sampling_rate, 51.0, 100.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            DataFilter.perform_bandpass(data[channel], self.sampling_rate, 51.0, 100.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            DataFilter.perform_bandstop(data[channel], self.sampling_rate, 50.0, 4.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            DataFilter.perform_bandstop(data[channel], self.sampling_rate, 60.0, 4.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            self.curves[count].setData(data[channel].tolist())
+
+        self.app.processEvents()
 
 
-board.prepare_session()
-# board.start_stream () # use this for default options
-board.start_stream(45000, None)
-time.sleep(2)
-# data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
-data = board.get_board_data()  # get all data and remove it from internal buffer
-board.stop_stream()
-board.release_session()
 
-print(data)
+def main():
+    BoardShim.enable_dev_board_logger()
+
+    params = BrainFlowInputParams()
+    params.ip_port = 0
+    params.serial_port = '/dev/cu.usbserial-DQ007SNX'
+    params.mac_address = ''
+    params.other_info = ''
+    params.serial_number = ''
+    params.ip_address = ''
+    params.ip_protocol = 0
+    params.timeout = 0
+    params.file = ''
+    
+    try:
+        board = BoardShim(0, params)
+        board.prepare_session()
+        # board.start_stream () # use this for default options
+        board.start_stream(45000, None)
+        Graph(board)
+    except BaseException:
+        logging.warning('Exception', exc_info=True)
+    finally:
+        logging.info('End')
+        if board.is_prepared():
+            logging.info('Releasing session')
+            board.release_session()
+            
+    #time.sleep(2)
+    # data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
+    #data = board.get_board_data()  # get all data and remove it from internal buffer
+    #board.stop_stream()
+    #board.release_session()
+    #board = BoardShim(0, params)
+
+if __name__ == '__main__':
+    main()
+
+
 
 
 
 # set default display to show all rows and columns:
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
+#pd.set_option('display.max_columns', None)
+#pd.set_option('display.max_rows', None)
 #pd.set_option('display.width', 250)
 
-data = pd.DataFrame(data)
+#data = pd.DataFrame(data)
 
-
-display(data)
 
 #print("Board Description: ", board.get_board_descr(0))
-print("Sampling rate: ",board.get_sampling_rate(0))
-print("EEG channels: ",board.get_eeg_channels(0))
-print("Accel. channels: ",board.get_accel_channels(0))
-print("Other channels: ",board.get_other_channels(0))
-print("timestamp channel: ", board.get_timestamp_channel(0))
+#print("Sampling rate: ",board.get_sampling_rate(0))
+#print("EEG channels: ",board.get_eeg_channels(0))
+#print("Accel. channels: ",board.get_accel_channels(0))
+#print("Other channels: ",board.get_other_channels(0))
+#print("timestamp channel: ", board.get_timestamp_channel(0))
 
 
 import datetime
@@ -83,8 +153,4 @@ def getChannels(data):
 #getRecord(data,3)
 
 
-data2 = getChannels(data)
-#display(data2)
-data2.plot()
-plt.show()
 
